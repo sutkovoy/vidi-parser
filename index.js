76d17dba8e2d95f90d8af8fc2348f942513c027a
Nikolay Sutkovoy <n.sutkovoy@gmail.com>
2017-02-11 22:38:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var cheerio = require('cheerio');
+var PushBullet = require('pushbullet'); //api for push notification
+var pusher = new PushBullet('o.k6Ad96wcQtbBNTddbpPH1uqrhy6UVwUy');
+var request = require("request");
+// var axios = require('axios');
+var Rx = require('rx');
+
+// var timeEvents = Rx.Observable.interval(300000); //get data every 5 min
+var timeEvents = Rx.Observable.interval(1000); //get data every 5 min
+
+timeEvents.subscribe(function () {
+  getData();
+});
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://nick885:5454541a6jc@ds149479.mlab.com:49479/vidi');
+
+var Schema = mongoose.Schema;
+
+var AutoSchema = new Schema({
+  carId    :  { type: String, default: '' },
+  carUrl   :  { type: String, default: '' },
+  carName  :  { type: String, default: '' },
+  carPrice :  { type: Number, default: 0 },
+  carPriceUsd :  { type: Number, default: 0 }
+});
+
+var autoModel = mongoose.model('Auto', AutoSchema);
+var url = 'http://vidi-automarket.com.ua/buy/';
+
+function getData() {
+  request(url, function (error, response, body) {
+    if (!error) {
+      var $ = cheerio.load(body);
+      $('.car').each(function (i, elem) {
+        var carId = elem.attribs.href.split('/')[3];
+        var carUrl = 'http://vidi-automarket.com.ua' + elem.attribs.href;
+        var carPrice = $(this).children('.price').text().replace(/[^0-9]/g, '');
+        var carPriceUsd = ($(this).children('.price').text().replace(/[^0-9]/g, '') / 27.7).toFixed();
+        var carName = $(this).children('.name').text();
+        var carDataObj = {carId: carId, carUrl: carUrl, carName: carName, carPrice: carPrice, carPriceUsd: carPriceUsd};
+
+        autoModel.findOne({carId: carId},function(err, data) {
+          if(!data) {
+            saveData(carDataObj);
+            sendPush(carDataObj);
+          }
+        })
+        
+      });
+      
+    } else {
+      console.log("Произошла ошибка: " + error);
+    }
+  });
+}
+
+function saveData(model) {
+  var auto = new autoModel(model);
+  auto.save(function (err, test) {});
+}
+
+function sendPush(data) {
+  var requestBody = `${data.carName} - ${data.carPrice} (${data.carPriceUsd} USD)`;
+  var requestUrl = data.carUrl;
+  pusher.link({}, requestBody, requestUrl, function (error, response) {
+    console.log('link sended');
+  });
+}
+
