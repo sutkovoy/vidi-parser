cb5fb25feb9354c08f0ebe6c6319191f94866e37
Nikolay Sutkovoy <n.sutkovoy@gmail.com>
2017-02-12 00:21:48 +0200

    Changed config

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var Rx = require('rx');
 
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(port, ipaddress, function() {
+  // Do your stuff
 });
 
 app.get('/', function (req, res) {
